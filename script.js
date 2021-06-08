3048afd8cb98114aa40e5bc8cacd69ff281703af
jazyyB <85590258+jazyyB@users.noreply.github.com>
2021-06-09 01:44:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("buttonDelete1").addEventListener
+("click", function() {
+document.getElementById("firstTable").hidden = true;
+document.getElementById("secondTable").hidden = false;
+}, false);
+
+document.getElementById("buttonDelete2").addEventListener
+("click", function() {
+document.getElementById("secondTable").hidden = true;
+document.getElementById("thirdTable").hidden = false;
+}, false);
+
+document.getElementById("buttonDelete2").addEventListener
+("click", function() {
+document.getElementById("secondTable").hidden = true;
+document.getElementById("thirdTable").hidden = false;
+}, false);
+
